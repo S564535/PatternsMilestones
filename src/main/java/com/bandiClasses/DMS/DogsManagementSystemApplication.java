f6f5e5c3cb4d8c495fa8f91efac0ea021e3e9d27
S564535 <143050181+S564535@users.noreply.github.com>
2024-04-20 12:53:21 -0500

    data added

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.bandiClasses.DMS;
+/**
+ * @author S564535 (Nithish Kumar Madavali)
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,11 @@
+package com.bandiClasses.DMS.repository;
+/**
+ * @author S564535 (Nithish Kumar Madavali)
+ */
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
